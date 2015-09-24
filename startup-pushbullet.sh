1f867d801e331dc2400027c15e82c4a1b6319329
Eric Goldman <goldmanej@gmail.com>
2015-09-24 04:32:23 -0400

    added formatting for multiple lines

--- startup-pushbullet.sh
+++ startup-pushbullet.sh
@@ -2,6 +2,7 @@
 
 PUSHBULLET_KEY=""
 NOTE_TITLE="Startup Notification"
-NOTE_BODY="Hostname: $(hostname)<br />IP: $(hostname -I)"
+NOTE_BODY="Hostname: $(hostname)\nIP: $(hostname -I)"
 
-curl -u $PUSHBULLET_KEY: https://api.pushbullet.com/v2/pushes -d type=note -d title="$NOTE_TITLE" -d body="$NOTE_BODY"
+#curl -u $PUSHBULLET_KEY: https://api.pushbullet.com/v2/pushes -d type=note -d title="$NOTE_TITLE" -d body="$NOTE_BODY"
+curl --header "Content-Type: application/json" --header "Access-Token: $PUSHBULLET_KEY" -X POST --data-binary '{"type":"note","title":"'"$NOTE_TITLE"'","body":"'"$NOTE_BODY"'"}' https://api.pushbullet.com/v2/pushes
